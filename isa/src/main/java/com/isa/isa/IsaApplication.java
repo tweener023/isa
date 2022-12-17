a685725883a474eed06c3b9b029e698cdc574bce
tweener023 <nikolajsatara99@gmail.com>
2022-12-17 14:00:29 +0100

    generated project

---
+++ isa/src/main/java/com/isa/isa/IsaApplication.java
@@ -0,0 +1,13 @@
+package com.isa.isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaApplication.class, args);
+	}
+
+}
