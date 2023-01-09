8ebadb975af951970070dfa055ac390cd4de36d3
tweener023 <nikolajsatara99@gmail.com>
2023-01-09 16:54:08 +0100

    added annotation for CORS problem

--- isa/isa/src/main/java/com/isa/controller/FacilityController.java
+++ isa/isa/src/main/java/com/isa/controller/FacilityController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "api/facilites")
 public class FacilityController {
