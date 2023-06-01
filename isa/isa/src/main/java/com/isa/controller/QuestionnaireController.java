5ca587a001404e14b01265e8f23723e2d9eeb748
SOgnjen <srdicognjen@gmail.com>
2023-06-01 16:42:05 +0200

    validations for submitting questionnaire

--- isa/isa/src/main/java/com/isa/controller/QuestionnaireController.java
+++ isa/isa/src/main/java/com/isa/controller/QuestionnaireController.java
@@ -2,6 +2,8 @@ package com.isa.controller;
 
 import com.isa.dto.QuestionnaireDTO;
 import com.isa.dto.UserDTO;
+import com.isa.model.BloodType;
+import com.isa.model.Gender;
 import com.isa.model.Questionnaire;
 import com.isa.model.User;
 import com.isa.service.QuestionnaireService;
@@ -12,8 +14,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestController
 @CrossOrigin
@@ -25,6 +30,86 @@ public class QuestionnaireController {
 
     @Autowired
     private UserService userService;
+
+    private boolean isValidDate(String date) {
+        String regex = "^\\d{4}-\\d{2}-\\d{2}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(date);
+        return matcher.matches();
+    }
+
+    private boolean isValidName(String name) {
+        String regex = "^[A-Z][a-z]*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+
+    private boolean isValidJMBG(String jmbg) {
+        String regex = "^[0-9]{8}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(jmbg);
+        return matcher.matches();
+    }
+
+    private boolean isValidGender(Gender gender) {
+        String genderStr = gender.toString();
+        String regex = "^(MALE|FEMALE)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(genderStr);
+        return matcher.matches();
+    }
+
+    private boolean isValidAddress(String address) {
+        String regex = "^[a-zA-Z0-9\\s,.-]*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(address);
+        return matcher.matches();
+    }
+
+    private boolean isValidCityName(String cityName) {
+        String regex = "^(?:[A-Z][a-z]*)(?:\\s[A-Z][a-z]*)*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(cityName);
+        return matcher.matches();
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        String regex = "^[0-9]{3}[-\\s]?[0-9]{3}[-\\s]?[0-9]{4}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+
+    private boolean isValidWorkplace(String workplace) {
+        String regex = "^[a-zA-Z0-9\\s.,'-]*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(workplace);
+        return matcher.matches();
+    }
+
+    private boolean isValidJob(String job) {
+        String regex = "^[a-zA-Z\\s.,'-]*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(job);
+        return matcher.matches();
+    }
+
+    private boolean isValidTimesGiven(String number) {
+        String regex = "^(0|[1-9]\\d*)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(number);
+        return matcher.matches();
+    }
+
+    private boolean isValidBloodType(BloodType bloodType) {
+        String genderStr = bloodType.toString();
+        String regex = "^(A|B|AB|O)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(genderStr);
+        return matcher.matches();
+    }
+
 
     @GetMapping(value = "/all")
     @PreAuthorize("hasAnyRole('USER', 'MEDIC', 'ADMINISTRATOR')")
@@ -65,6 +150,69 @@ public class QuestionnaireController {
     @PostMapping(value = "/submit")
     @PreAuthorize("hasAnyRole('USER', 'MEDIC', 'ADMINISTRATOR')")
     public ResponseEntity<QuestionnaireDTO> submitQuestionnaire(@RequestBody QuestionnaireDTO questionnaireDTO) {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateStr1 = dateFormat.format(questionnaireDTO.getDateOfQuestionnaire());
+        String dateStr2 = dateFormat.format(questionnaireDTO.getDateOfBirth());
+
+        if (!isValidDate(dateStr1)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidDate(dateStr2)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidName(questionnaireDTO.getFirstName())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidName(questionnaireDTO.getParentName())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidName(questionnaireDTO.getLastName())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+
+        String jmbgStr = String.valueOf(questionnaireDTO.getJmbg());
+
+        if (!isValidJMBG(jmbgStr)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidGender(questionnaireDTO.getGender())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidAddress(questionnaireDTO.getAddress())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidCityName(questionnaireDTO.getCity())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidPhoneNumber(questionnaireDTO.getPhoneNumber())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidBloodType(questionnaireDTO.getBloodType())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidWorkplace(questionnaireDTO.getWorkplace())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidJob(questionnaireDTO.getJob())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (!isValidTimesGiven(Integer.toString(questionnaireDTO.getTimesGiven()))) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         // Retrieve the user by user ID from the questionnaire DTO
         User user = userService.findOne(questionnaireDTO.getUserId());
 
