e592d167549ddbcc494b0e8c8e0490f6a5dd57c9
tweener023 <nikolajsatara99@gmail.com>
2023-06-21 15:16:03 +0200

    grpc initial setup

--- isa/isa/src/main/java/com/isa/grpc/CommunicationService.java
+++ isa/isa/src/main/java/com/isa/grpc/CommunicationService.java
@@ -1,4 +1,22 @@
 package com.isa.grpc;
 
-public class CommunicationService {
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.util.UUID;
+
+@GrpcService
+public class CommunicationService extends com.isa.grpc.SpringGrpcServiceGrpc.SpringGrpcServiceImplBase {
+
+    @Override
+    public void communicate(com.isa.grpc.MessageProto request, StreamObserver<com.isa.grpc.MessageResponseProto> responseObserver){
+        System.out.println("Message: " + request.getMessage() + "; randomInteger: " + request.getRandomInteger());
+
+        com.isa.grpc.MessageResponseProto responseMessage = com.isa.grpc.MessageResponseProto.newBuilder()
+                .setResponse("Spring Boot: " + UUID.randomUUID().toString()).setStatus("Status 200").build();
+
+        responseObserver.onNext(responseMessage);
+        responseObserver.onCompleted();
+    }
+
 }
