85cd75733ebde728e57d5e6a1a3a10a2599baf2c
tweener023 <nikolajsatara99@gmail.com>
2023-06-26 20:36:15 +0200

    createAppointment for grpc service

--- isa/isa/src/main/java/com/isa/grpc/CommunicationService.java
+++ isa/isa/src/main/java/com/isa/grpc/CommunicationService.java
@@ -4,15 +4,15 @@ import com.google.protobuf.Timestamp;
 import com.isa.model.Appointments;
 import com.isa.model.Facility;
 import com.isa.service.FacilityService;
+import com.isa.service.UserService;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneOffset;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -23,6 +23,9 @@ public class CommunicationService extends com.isa.grpc.SpringGrpcServiceGrpc.Spr
 
     @Autowired
     FacilityService facilityService;
+
+    @Autowired
+    UserService userService;
 
     @Override
     public void communicate(com.isa.grpc.MessageProto request, StreamObserver<com.isa.grpc.MessageResponseProto> responseObserver) {
@@ -105,6 +108,51 @@ public class CommunicationService extends com.isa.grpc.SpringGrpcServiceGrpc.Spr
         return appointmentsDTO;
     }
 
+    @Override
+    public void createAppointment(com.isa.grpc.CreateAppointmentRequest request, StreamObserver<com.isa.grpc.AppointmentDTO> responseObserver) {
+        int userId = request.getUserId();
+        com.isa.grpc.AppointmentDTO appointmentDto = request.getAppointment();
+
+        // Retrieve the user using userId
+        com.isa.model.User user = userService.findOne(userId);
+
+        if (user == null) {
+            responseObserver.onError(Status.NOT_FOUND.withDescription("User not found").asRuntimeException());
+            return;
+        }
+
+        if (!user.isFilledQuestionnaire()) {
+            responseObserver.onError(Status.PERMISSION_DENIED.withDescription("User has not filled the questionnaire").asRuntimeException());
+            return;
+        }
+
+        com.isa.grpc.AppointmentDTO.Builder appointmentBuilder = appointmentDto.toBuilder();
+
+        // Create a new Appointments object
+        com.isa.model.Appointments appointment = new com.isa.model.Appointments();
+
+        // Set the appointment properties based on appointmentDto
+        appointment.setAppointmentId(appointmentDto.getAppointmentId());
+        appointment.setUser(user);
+        appointment.setDateOfAppointment(convertTimestampToLocalDate(appointmentDto.getDateOfAppointment()));
+        appointment.setTimeOfAppointment(convertTimestampToLocalTime(appointmentDto.getTimeOfAppointment()));
+        // Set other appointment fields...
+
+
+        // Add the appointment to the user's appointments list
+        user.getAppointments().add(appointment);
+
+        // Save the updated user
+        user = userService.save(user);
+
+        // Build the response message
+        com.isa.grpc.AppointmentDTO response = appointmentBuilder.build();
+
+        // Send the response
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
     // Helper method to convert LocalDate to Timestamp
     private Timestamp convertLocalDateToTimestamp(LocalDate localDate) {
         return Timestamp.newBuilder()
@@ -118,5 +166,17 @@ public class CommunicationService extends com.isa.grpc.SpringGrpcServiceGrpc.Spr
                 .setNanos(localTime.toSecondOfDay() * 1_000_000_000)
                 .build();
     }
-    
+    // Helper method to convert Timestamp to LocalDate
+    private LocalDate convertTimestampToLocalDate(com.google.protobuf.Timestamp timestamp) {
+        Instant instant = Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        return localDateTime.toLocalDate();
+    }
+
+    // Helper method to convert Timestamp to LocalTime
+    private LocalTime convertTimestampToLocalTime(com.google.protobuf.Timestamp timestamp) {
+        Instant instant = Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        return localDateTime.toLocalTime();
+    }
 }
